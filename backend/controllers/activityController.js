12b927db29931d1f8a03a8a973a8ba26012f4068
Pedro Henrique de Mesquita <13134297671@MUZAMBINHO.EDU.BR>
2026-03-10 22:46:16 -0300

    feat: adicionado cálculo de distância e envio de coordenadas GPS nas atividades

--- backend/controllers/activityController.js
+++ backend/controllers/activityController.js
@@ -1,4 +1,5 @@
 const { createActivity, getActivities } = require("../models/activityModel");
+const calculateDistance = require("../utils/distance");
 
 function startActivity(req, res) {
   const { userId } = req.body;
@@ -21,4 +22,46 @@ function getAllActivities(req, res) {
   res.json(activities);
 }
 
-module.exports = { startActivity, getAllActivities };
+function addLocation(req, res) {
+
+  const { activityId, latitude, longitude } = req.body;
+
+  const activities = getActivities();
+
+  const activity = activities.find(a => a.id == activityId);
+
+  if (!activity) {
+    return res.status(404).json({ message: "Atividade não encontrada" });
+  }
+
+  const lastCoordinate = activity.coordinates[activity.coordinates.length - 1];
+
+  if (lastCoordinate) {
+
+    const distance = calculateDistance(
+      lastCoordinate.latitude,
+      lastCoordinate.longitude,
+      latitude,
+      longitude
+    );
+
+    activity.distance += distance;
+  }
+
+  activity.coordinates.push({
+    latitude,
+    longitude,
+    time: new Date()
+  });
+
+  res.json({
+    message: "Localização adicionada",
+    totalDistance: activity.distance
+  });
+}
+
+module.exports = { 
+  startActivity, 
+  getAllActivities,
+  addLocation
+};
--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { startActivity, getAllActivities } = require("../controllers/activityController");
+const { startActivity, getAllActivities, addLocation } = require("../controllers/activityController");
 
 router.post("/start", startActivity);
 router.get("/", getAllActivities);
+router.post("/location", addLocation);
 
 module.exports = router;
