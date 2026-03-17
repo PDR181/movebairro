74b65ccd3061153d85afd94b345eeda775b7f198
Pedro Henrique de Mesquita <13134297671@MUZAMBINHO.EDU.BR>
2026-03-17 20:19:11 -0300

    feat: integrado frontend com GPS e registro de atividades

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const userRoutes = require("./routes/userRoutes");
 const activityRoutes = require("./routes/activityRoutes");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
